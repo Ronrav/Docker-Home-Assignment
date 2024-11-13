d6ef7a7b37a835fef34e9e5cd19ef4c598ee40c5
Ron_Raviv <ronraviv37@gmail.com>
2024-11-13 16:03:52 +0200

    add execution permission

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -8,7 +8,7 @@ else
     exit 1
 fi
 
-# Use envsubst to replace placeholders in nginx.conf.template and output to nginx.conf
+# Use envsubst to replace placeholders in nginx.conf.template and output nginx.conf
 envsubst '${SERVER_PORT_HTML} ${SHARED_DIR} ${CUSTOM_HTML_FILE} ${SERVER_PORT_ERROR} ${HTTP_ERROR_CODE}' < nginx/nginx.conf.template > nginx/nginx.conf
 
 echo "nginx.conf has been configured successfully!"
